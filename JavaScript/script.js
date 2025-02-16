a6a15da8d4060694aca2c44d6a0959b014c96f0e
Zaheer Ali <zaheerrahman0@gmail.com>
2025-02-16 23:40:58 +0500

    JavaScript Introduction

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+// console.log('Web Dev');
+// document.write("<h1>JavaScript</h1>");
+// alert('Welcome');
+// prompt('Hello');
+
+// ============= Variables =============
+// var
+// let 
+// const
+
+// var a = 500;
+// let a = 500;
+// let b = 1000;
+// b = 500;
+// console.log(b)
+
+// const z = 10;
+// z = 20;
+// console.log(z);
