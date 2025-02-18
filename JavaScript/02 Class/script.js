05eb2549784bbd0e455c392ee713057748ee5935
Zaheer Ali <zaheerrahman0@gmail.com>
2025-02-18 22:51:18 +0500

    JavaScript String

---
+++ JavaScript/02 Class/script.js
@@ -0,0 +1,22 @@
+// ================== String Topic ==================
+// let string = "Hello String";
+// let stringTwo = "10 + 10";
+// let stringThree = new String("Hello");
+// let num = 20;
+// let a = true;
+// let stringFour = `Hello World ${num} Web Tech ${a}`;
+// console.log(stringFour);
+
+// length
+// let a = 'AI Tech'
+// console.log(a.length);
+
+// let string = 'Frontend Developer ';
+// let two = 'AI';
+// let printer = string.toUpperCase();
+// let printer = string.toLowerCase();
+// let printer = string.charAt(3);
+// let printer = string.includes('s');
+// let printer = string.concat(two);
+
+// console.log(printer);
