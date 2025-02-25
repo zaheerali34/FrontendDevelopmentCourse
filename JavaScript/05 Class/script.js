8ac82ccdab2908691faf65d52395ec6e2ff3f949
Zaheer Ali <zaheerrahman0@gmail.com>
2025-02-25 22:39:34 +0500

    loop

---
+++ JavaScript/05 Class/script.js
@@ -0,0 +1,47 @@
+// ============== Loop ==============
+// let start = 0;
+// let end = 10;
+
+// for (let i = start; i <= end; i++) {
+//     console.log(i);
+// }
+
+// for (let a = 1; a <= 10; a++) {
+//     console.log(a);
+// }
+
+// while Loop
+// let num = 0;
+
+// while (num <= 5) {
+//     console.log('Count is : ' + num);
+//     num++;
+// }
+
+// do while loop
+
+// let a = 0;
+
+// do {
+//     console.log('Hello : ' + a);
+//     a++;
+    
+// } while(a <= 10);
+
+// for in loop > Object
+// const obj = {
+//     name: 'ali',
+//     id: 4334,
+//     numbers: 23232,
+// }
+
+// for (let key in obj) {
+//     console.log(obj[key]);
+// }
+
+// for of loop > Array
+// let arr = [1, 2, 3, 4, 5, 'HTML', 'CSS'];
+
+// for (let i of arr) {
+//     console.log(i);
+// }
