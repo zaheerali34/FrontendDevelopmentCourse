c32d980e80ff9d71c7ab1ca891e7e132d7dac59a
Zaheer Ali <zaheerrahman0@gmail.com>
2025-02-27 22:55:25 +0500

    parameters and arguments

---
+++ JavaScript/06 Class/script.js
@@ -0,0 +1,45 @@
+// ============== Function ==============
+// function getFun() {
+//     console.log('Hello');
+// }
+
+// getFun();
+// getFun();
+// getFun();
+
+// const Hello = function () {
+//     console.log('Hello');
+// }
+
+// Hello();
+
+// function print() {
+//     let b = 'hello'
+//     console.log(b);
+//     return 100;
+// }
+
+// let a = print();
+// console.log(a);
+
+// parameters and arguments
+// function getFun(a) {
+//     console.log(a);
+// }
+
+// getFun(100);
+
+// function add(a, b) {
+//     return a * b;
+// }
+
+// let print = add(10, 40); 
+// console.log(print);
+
+// function three(a, b = 'World') {
+//     return a + b 
+// }
+
+// let a = three('Hello ');
+// console.log(a);
+
