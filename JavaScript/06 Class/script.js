5597fde8d34ade8e6f44d51689af0d2e1547f614
Zaheer Ali <zaheerrahman0@gmail.com>
2025-02-28 22:09:07 +0500

    function

--- JavaScript/06 Class/script.js
+++ JavaScript/06 Class/script.js
@@ -33,13 +33,58 @@
 //     return a * b;
 // }
 
-// let print = add(10, 40); 
+// let print = add(10, 40);
 // console.log(print);
 
 // function three(a, b = 'World') {
-//     return a + b 
+//     return a + b
 // }
 
 // let a = three('Hello ');
 // console.log(a);
 
+// rest parameters => array []
+// function getHello(...a) {
+//     return a;
+// }
+
+// const print = getHello('Hello', 'World', 'HTML');
+// console.log(print);
+
+// Nested Function
+// function one() {
+//     let a = 'One';
+//     console.log(a);
+//     function tow() {
+//         let b = 'two';
+//         console.log(b);
+//     }
+//     tow();
+// }
+
+// one();
+
+// arrow function
+// const arrow = (a, b) => {
+//     return a + b;
+// }
+
+// let print = arrow(10, 20);
+// console.log(print);
+
+// const one = () => console.log('One');
+// one();
+
+// const two = (a, b) => a + b;
+// console.log(two(10, 40));
+
+// Immediately Invoked Function Expression(IIFE)
+// (function () {
+//     console.log('JavaScript')
+// })();
+
+// (
+//     function () {
+//         console.log('Web Tech');
+//     }
+// )();
