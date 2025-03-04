0504a41a2fdd0cadb9e70593a180e2432b461686
Zaheer Ali <zaheerrahman0@gmail.com>
2025-03-04 23:33:18 +0500

    array

--- JavaScript/06 Class/script.js
+++ JavaScript/06 Class/script.js
@@ -88,3 +88,4 @@
 //         console.log('Web Tech');
 //     }
 // )();
+
---
+++ JavaScript/07 Class/script.js
@@ -0,0 +1,42 @@
+// ================= Array ==================
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// let one = new Array(1, 2, 3, 4, 5);
+// console.log(one);
+
+// let two = ['HTML', 'CSS', 1, 2, 3, true, {name: 'ali'}];
+// console.log(two);
+
+// let three = [
+//     {
+//         name: 'ali khan'
+//     },
+
+//     {
+//         id: 12234,
+//     }
+// ];
+
+// console.log(three);
+
+// Functions on Arrays 
+// let arr = ['HTML', 'CSS', 'JAVASCRIPT'];
+// push
+// arr.push('React Js');
+
+// pop
+// arr.pop();
+// console.log(arr);
+
+// shift
+// arr.shift();
+// console.log(arr);
+
+// unshift
+// arr.unshift('C++');
+// console.log(arr);
+
+// indexOf
+// let a = arr.indexOf('JAVASCRIPT');
+// console.log(a);
