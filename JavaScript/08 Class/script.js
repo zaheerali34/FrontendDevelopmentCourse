0c3a795b5c8192dd2b962b6103a2fd057e719c64
Zaheer Ali <zaheerrahman0@gmail.com>
2025-03-06 23:56:04 +0500

    object

---
+++ JavaScript/08 Class/script.js
@@ -0,0 +1,45 @@
+// =============== Object ===============
+// const user = {
+//     name: 'ali',
+//     id: 2356,
+//     login: true
+// }
+
+// let print = user.name;
+// console.log(print);
+
+// const one = {
+//     name: "khan",
+//     "course": 'JavaScript',
+//     ['id']: 1350,
+// }
+
+// console.log(one.id);
+
+// const three = {
+//     name: 'khan',
+//     arr: ['HTML', 'CSS', 'JS'],    
+//     fun: function () {
+//         console.log('Hello');        
+//     }
+// }    
+
+// console.log(three.fun());
+
+// const one = {
+//     name: 'ali',
+//     last: {
+//         lastName: 'khan',
+//         id: 23545,
+//     }
+// }
+
+// console.log(one.last.lastName);
+
+// const obj = {};
+
+// obj.name = "ali";
+// obj.course = 'js';
+// obj.id = 2345;
+
+// console.log(obj);
