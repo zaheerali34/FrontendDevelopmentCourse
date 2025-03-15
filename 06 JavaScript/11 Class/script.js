a97cbd19d718d01f22c72c29ab93641c70c837bd
Zaheer Ali <zaheerrahman0@gmail.com>
2025-03-16 00:06:59 +0500

    Promise

---
+++ 06 JavaScript/11 Class/script.js
@@ -0,0 +1,45 @@
+// =============== Synchronous ================
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// let a = 10;
+// console.log(a);
+
+// let b = 'hello'
+// console.log(b);
+
+// =============== Asynchronous ================
+// console.log(1);
+// console.log(2);
+
+// setTimeout(function (){
+//     console.log(3);
+// }, 2000);
+
+// console.log(4);
+// console.log(5);
+
+// const data = new Promise(function (resolve,reject){
+// resolve('Hello');
+//     reject('this is error'); // error
+// });
+
+// console.log(data);
+
+// const failedPromise = new Promise((resolve, reject) => {
+//     setTimeout(function (){
+//         reject('Error');
+//         // resolve('Hello World')
+//     }, 2000)
+// });
+
+// failedPromise.then(function (res){
+//     console.log(res);
+// })
+
+// failedPromise.catch(function (error){
+//     console.log(error);
+// })
