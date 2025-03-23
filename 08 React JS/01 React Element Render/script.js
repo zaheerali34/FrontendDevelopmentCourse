d40f311bed8470856b789c0964454186daa563e9
Zaheer Ali <zaheerrahman0@gmail.com>
2025-03-23 23:27:14 +0500

    React Element Render

---
+++ 08 React JS/01 React Element Render/script.js
@@ -0,0 +1,21 @@
+// JavaScript Create Element
+// const tag = document.querySelector('#tag');
+
+// let a = document.createElement('h1');
+// a.innerHTML = 'Hello World JavaScript';
+
+// tag.appendChild(a);
+
+// ======================================================================
+// React Create Element
+// const h1 = React.createElement('h1', {className: 'tag'}, 'Hello World');
+
+// const root = ReactDOM.createRoot(document.querySelector('#root'));
+// root.render(h1);
+
+// JSX 
+// let arr = ['HTML', 'CSS', 'JavaScript'];
+// const h1 = <h1>Hello World JSX: {arr[1]}</h1>;
+
+// const root = ReactDOM.createRoot(document.querySelector('#root'));
+// root.render(h1);
